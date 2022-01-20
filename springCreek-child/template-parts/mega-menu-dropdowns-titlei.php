8d7f590e5b65be1f50bcfde054ed12795716e049
Espi <joshespi@gmail.com>
2022-01-20 14:48:24 -0700

    first upload

---
+++ springCreek-child/template-parts/mega-menu-dropdowns-titlei.php
@@ -0,0 +1,78 @@
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="int"><a href="https://springcreek.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/2016-17-Pamphlet-Annual-Title-I-Information-for-Parents.pdf">Annual Title I Information for Parents</a></li>
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+						</ul>
+						<p>Title I Coordinator: Michelle Blair (michellebl@provo.edu)</p>
+				</div>
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2021/10/SC_ParentCompact_fy22_Eng-1.pdf">Parent-School Learning Compact (English)</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2021/10/SC_ParentCompact_fy22_Spa-1.pdf">Parent-School Learning Compact (Spanish)</a></li>
+						</ul>
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Engagement Policy</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Engagement Policy</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2021/10/SC_ParentEngagement_fy22_Eng-1.pdf">Parent Engagement Policy (English)</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2021/10/SC_ParentEngagement_fy22_Spa-1.pdf">Parent Engagement Policy (Spanish)</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+							<?php
+						}
+					?>
+						<ul>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2018/12/Parent-Right-to-Know-EngSpan-.pdf">Parent Right to Know</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2019/10/SC_AM_November_FY20_TI-Annual-Meeting-slides.pdf">Annual Title I Meeting Info</a></li>
+							<li class="pdf"><a href="https://utahschoolgrades.schools.utah.gov/Home/SchoolOverAllPerformance?SchoolID=45549&DistrictID=1222&SchoolNbr=123&SchoolLevel=K8&IsSplitSchool=0&schoolyearendyear=2021">School Report Card</a></li>
+							<li class="ext"><a href="https://public.tableau.com/shared/PBR64BGZQ?:display_count=n&:origin=viz_share_link">Comparable Schools Report</a></li>
+							<li class="pdf"><a href="https://springcreek.provo.edu/wp-content/uploads/2020/11/Spring-Creek-Title-I-FY21-Goals-at-a-glance.pdf"><span class="hidden">Spring Creek Elementary </span>Goals At A Glance Fy21</a></li>
+						</ul>
+				</div>
